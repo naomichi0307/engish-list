e173bb86251669734244986d302a146fa9e9a22a
naomichi <shibata19970307@gmail.com>
2022-08-12 15:26:41 +0900

    elapp修正

--- elproject/elapp/models.py
+++ elproject/elapp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 PRIORITY=(('danger','high'),('info','middle'),('success','low'))
 class elModel(models.Model):
     word=models.CharField(max_length=25)
-    mean=models.CharField(max_length=100)
+    #mean=models.CharField(max_length=100)
     priority= models.CharField(
         max_length=50,
         choices = PRIORITY
