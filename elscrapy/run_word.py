d287e8c8a1613f12708c2ffb9555e969ef715efc
naomichi <shibata19970307@gmail.com>
2022-08-28 12:58:08 +0900

    全部コミット

---
+++ elscrapy/run_word.py
@@ -0,0 +1,7 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+process = CrawlerProcess(settings=get_project_settings())
+process.crawl('word')
+process.start()
